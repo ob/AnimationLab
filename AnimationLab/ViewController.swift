54217d688f3007be189310554f376244585302b8
Oscar Bonilla <6f6231@gmail.com>
2017-10-04 18:24:19 -0700

    Milestone 2

--- AnimationLab/ViewController.swift
+++ AnimationLab/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trayView: UIView!
+
+    var trayOriginalCenter: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        trayOriginalCenter = trayView.center
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func trayDragged(_ sender: UIPanGestureRecognizer) {
+        let point = sender.location(in: trayView.superview)
+
+        if sender.state == .began {
+        } else if sender.state == .changed {
+            let translation = sender.translation(in: trayView.superview)
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+            print("Gesture changed at: \(point)")
+        } else if sender.state == .ended {
+            trayOriginalCenter = trayView.center
+            print("Gesture ended at: \(point)")
+        }
+    }
 
 }
 
